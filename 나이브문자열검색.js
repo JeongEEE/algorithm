ccff0a1cd18af4ad28ad10c599023c2f99addca9
Gyu Jeong Kim <tbtbtb156@naver.com>
2022-09-28 15:18:45 +0900

    나이브 문자열 검색

---
+++ 나이브문자열검색.js
@@ -0,0 +1,16 @@
+// Search Naive String
+function naiveSearch(long, short) {
+	var count = 0;
+	for(var i = 0; i < long.length; i++) {
+		for(var j = 0; j < short.length; j++) {
+			if(short[j] !== long[i+j]) break;
+			if(j === short.length - 1) {
+				console.log('Found!');
+				count++;
+			}
+		}
+	}
+	return count;
+}
+
+naiveSearch('lorie loled', 'lol')
