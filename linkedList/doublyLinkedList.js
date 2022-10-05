6cc7ceb82c33143f3baada3c956da6b1ec7abc8e
Gyu Jeong Kim <tbtbtb156@naver.com>
2022-10-05 14:10:18 +0900

    doublyLinkedList remove

--- linkedList/doublyLinkedList.js
+++ linkedList/doublyLinkedList.js
@@ -100,7 +100,7 @@ class DoublyLinkedList {
 		if(index < 0 || index > this.length) return false;
 		if(index === 0) return this.unshift(val);
 		if(index === this.length) return this.push(val);
-		
+
 		var newNode = new Node(val);
 		var beforeNode = this.get(index - 1);
 		var afterNode = beforeNode.next;
@@ -110,6 +110,21 @@ class DoublyLinkedList {
 		afterNode.prev = newNode;
 		this.length++;
 		return true;
+	}
+	remove(index) {
+		if(index < 0 || index >= this.length) return undefined;
+		if(index === 0) return this.shift();
+		if(index === this.length - 1) return this.pop();
+
+		var removedNode = this.get(index);
+		var beforeNode = removedNode.prev;
+		var afterNode = removedNode.next;
+		beforeNode.next = afterNode;
+		afterNode.prev = beforeNode;
+		removedNode.next = null;
+		removedNode.prev = null;
+		this.length--;
+		return removedNode;
 	}
 }
 
