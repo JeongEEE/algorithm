9edc5331fe385bd7c67b89146e5176353c69e21d
Gyu Jeong Kim <tbtbtb156@naver.com>
2022-10-17 14:49:28 +0900

    graph dfs

--- graph.js
+++ graph.js
@@ -24,9 +24,50 @@ class Graph {
 		}
 		delete this.adjacencyList[vertex]
 	}
+	depthFirstRecursive(start) {
+		const result = [];
+		const visited = {};
+		const adjacencyList = this.adjacencyList;
+
+		(function dfs(vertex) {
+			if(!vertex) return null;
+			visited[vertex] = true;
+			result.push(vertex);
+			adjacencyList[vertex].forEach(neighbor => {
+				if(!visited[neighbor]) {
+					return dfs(neighbor)
+				}
+			})
+		})(start);
+
+		return result;
+	}
+	depthFirstIterative(start) {
+		const stack = [start];
+		const result = [];
+		const visited = {};
+		let currentVertex;
+
+		visited[start] = true;
+		while(stack.length) {
+			currentVertex = stack.pop();
+			result.push(currentVertex);
+
+			this.adjacencyList[currentVertex].forEach(neighbor => {
+				if(!visited[neighbor]) {
+					visited[neighbor] = true;
+					stack.push(neighbor);
+				}
+			})
+		}
+		return result;
+	}
 }
 
 let g = new Graph();
-g.addVertex('Seoul');
-g.addVertex('Dokyo');
-g.addVertex('Dallas');
+g.addVertex('A');
+g.addVertex('B');
+g.addVertex('C');
+g.addVertex('D');
+g.addVertex('E');
+g.addVertex('F');
