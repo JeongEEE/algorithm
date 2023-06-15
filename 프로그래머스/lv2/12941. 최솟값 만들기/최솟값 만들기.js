d790a7c304b374985ac4037240a9f46f018bb3d4
Gyu Jeong <51220239+JeongEEE@users.noreply.github.com>
2023-06-15 14:32:20 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.75 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -0,0 +1,6 @@
+function solution(A,B) {
+    // 각 배열의 원소를 곱해서 더했을 때 최솟값을 만들기 위해서는 (가장 작은 수 * 가장 큰 수)를 곱한 수를 더하면 된다.
+    A.sort((a, b) => a - b);
+    B.sort((a, b) => b - a);
+    return A.reduce((sum, curr, i) => sum + curr * B[i], 0);
+}
